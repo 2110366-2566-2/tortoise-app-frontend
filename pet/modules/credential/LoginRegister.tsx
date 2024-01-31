80c490df7b38e9b21b968e203408f8e039ba628c
Ruthless <rutthee9999@gmail.com>
2024-01-31 21:14:07 +0700

    [TL-4, TL-5, TL-11] Setting Path, Implement Search Box

---
+++ pet/modules/credential/LoginRegister.tsx
@@ -0,0 +1,87 @@
+'use client';
+import { Tabs, Tab, TextField, Typography, Box } from '@mui/material';
+import { useState } from 'react';
+import LoginIcon from '@mui/icons-material/Login';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import LoginLeftFrame from '../../public/image/login_hero.png';
+import Image from 'next/image';
+import LoginForm from '../login/LoginForm';
+import RegisterForm from '../register/RegisterForm';
+
+export default function LoginRegisterPage() {
+    const [value, setValue] = useState(0);
+
+    const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '100vw',
+                height: '100vh',
+                backgroundColor: '#FFD4B7',
+            }}
+        >
+            <Box
+                sx={{
+                    width: '60%',
+                    height: '70%',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    zIndex: 1,
+                    borderRadius: '4px',
+                    boxShadow: 20,
+                }}
+            >
+                <Box
+                    sx={{
+                        width: '64%',
+                        height: '100%',
+                        backgroundColor: '#EEBC97',
+                        position: 'relative',
+                        display: 'block',
+                    }}
+                >
+                    <Image
+                        src={LoginLeftFrame}
+                        alt="login_frame"
+                        fill={true}
+                        sizes="100%"
+                        placeholder="blur"
+                        blurDataURL={''}
+                        style={{ objectFit: 'cover', objectPosition: 'bottom' }}
+                    />
+                    <Typography
+                        sx={{
+                            py: '30%',
+                            fontWeight: 'bold',
+                            fontSize: '20px',
+                            textAlign: 'center',
+                            whiteSpace: 'pre-line',
+                            overflow: 'hidden',
+                        }}
+                    >
+                        {`WHERE PETS FIND\nTHEIR NEW FAMILIES`}
+                    </Typography>
+                </Box>
+                <Box
+                    sx={{
+                        backgroundColor: '#FFFEED',
+                        width: '36%',
+                        height: '100%',
+                        display: 'block',
+                    }}
+                >
+                    <Tabs value={value} onChange={handleTabChange} aria-label="icon label tabs example">
+                        <Tab icon={<LoginIcon />} label="LOGIN" sx={{ width: '50%' }} />
+                        <Tab icon={<PersonAddIcon />} label="REGISTER" sx={{ width: '50%' }} />
+                    </Tabs>
+                    <Box>{value === 0 ? <LoginForm /> : <RegisterForm />}</Box>
+                </Box>
+            </Box>
+        </Box>
+    );
+}
---
+++ pet/modules/marketplace/MarketplacePage.tsx
@@ -0,0 +1,19 @@
+import { Box, Typography } from '@mui/material';
+import PetCatalogue from '../../components/PetCatalogue';
+import SearchField from '../../components/SearchField';
+
+export default function MarketplacePage() {
+    return (
+        <div>
+            <Typography variant="h5" sx={{ textAlign: 'center', fontWeight: 800, p: '32px 10% 8px' }}>
+                Your Ideal Pet Awaits
+            </Typography>
+            <Box sx={{ p: '16px 10%', textAlign: '-webkit-center' }}>
+                <SearchField />
+            </Box>
+            <div className="mt-8 mx-8">
+                <PetCatalogue />
+            </div>
+        </div>
+    );
+}
