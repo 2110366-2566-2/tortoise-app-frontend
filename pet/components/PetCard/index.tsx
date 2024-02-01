e9927446e9c855f926faa42efa4a99d679316426
khunnnnnnn <nunnun.234@gmail.com>
2024-02-01 18:47:29 +0700

    [TL-24] PetCard Styling

--- pet/components/PetCard/index.tsx
+++ pet/components/PetCard/index.tsx
@@ -1,18 +1,36 @@
+import { Box } from "@mui/material"
 import Image, { StaticImageData } from "next/image"
 
 export default function PetCard({petName, imgSrc} : {petName: string, imgSrc: StaticImageData}) {
 
     return(
-        <div className="m-8 w-1/5 h-[300px] shadow-xl border border-2 border-black rounded-lg">
-            <div className="w-full h-[70%] relative rounded-t-lg">
+        <div style={{border: '2px solid black', boxShadow: '5px 4px #472F05'}}>
+        {/* <div className="m-8 w-1/5 h-[300px] shadow-xl border border-dashed border-black"> */}
+            <div>
                 <Image src={imgSrc}
                     alt="Pet Picture"
-                    width={300}
+                    width={500}
                     height={300}
-                    className="object-fit rounded-lg"
+                    className="object-fit rounded-t-xl border-b"
                     />
             </div>
-            <div className="text-center text-xl font-bold">{petName}</div>
+            <div style={{padding: 6, textAlign: 'center'}}>{petName}</div>
+            <div></div>
+            <div className="flex flex-row justify-around">
+                <div></div>
+            </div>
         </div>
+
+        // <Box sx={{margin: 4, height: '300px', boxShadow: 10, border: '2px solid grey'}}>
+        //     <Box sx={{height: '500px', width: '300px'}}>
+        //         <Image src={imgSrc}
+        //             alt="Pet Picture"
+        //             width={500}
+        //             height={300}
+        //             className="object-cover rounded-t-xl"
+        //             />
+        //     </Box>
+
+        // </Box>
     )
 }
--- pet/components/PetCatalogue/index.tsx
+++ pet/components/PetCatalogue/index.tsx
@@ -36,7 +36,7 @@ export default function PetCatalogue() {
 
     return (
         <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} justifyContent='space-around'>
                 {mockData.map((eachpetCard, index) => (
                     <Grid item xs={2} sm={4} md={4} key={index}>
                         <PetCard petName={eachpetCard.petName} imgSrc={eachpetCard.imgSrc} />
