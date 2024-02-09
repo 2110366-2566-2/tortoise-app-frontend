83cd9a7a51c8b1174b9b5fe2e89518acb91c197b
Apiwat Saejung <apiwatsaejung@gmail.com>
2024-02-09 21:30:37 +0700

    PetProfile&SellerShopPage

--- pet/src/app/user/Test/page.tsx
+++ pet/src/app/user/Test/page.tsx
@@ -1,19 +1,7 @@
 'use client';
-import { Tabs, Tab, TextField, Typography, Box } from '@mui/material';
-import DogPic from '../../../../public/image/mockdog1.jpg';
-import { Image } from '@mui/icons-material';
-import TopBar from '../../../../components/TopBar';
-import ProfileCard from '../../../../components/ProfileCard.tsx';
-import SettingsCard from '../../../../components/SettingsCard';
-import PetCard from '../../../../components/PetCard';
-import PetCatalog from '../../../../components/PetCatalogue';
-import SearchField from '../../../../components/SearchField';
-import PetCatalogue from '../../../../components/PetCatalogue';
+import CatalogueBySeller from "../../../../components/CatalogueBySeller";
+import SettingsCard from "../../../../components/SettingsCard";
 export default function Test() {
-    return <div><TopBar/>
-    <div><ProfileCard/>
-    <div><SettingsCard/></div>
-    </div>
-    </div>
+    return  <div><CatalogueBySeller/></div>
 
 }
