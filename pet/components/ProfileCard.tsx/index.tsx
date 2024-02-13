2c6eb3dd134203de82530da738ec158857f483df
khunnnnnnn <nunnun.234@gmail.com>
2024-02-13 19:20:59 +0700

    [FIX] Restyling PetProfilePage

--- pet/components/ProfileCard.tsx/index.tsx
+++ pet/components/ProfileCard.tsx/index.tsx
@@ -1,12 +1,17 @@
-'use client';
 // IMPORTS
 import Card from '@mui/material/Card';
 import Typography from '@mui/material/Typography';
-import { Grid } from '@mui/material';
+import { Box, Grid } from '@mui/material';
+import Image from 'next/image';
 import Avatar from '@mui/material/Avatar';
 import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
 import Badge from '@mui/material/Badge';
+import useGetPetByID from '../../services/api/v1/pets/useGetPetByID';
+import { Fira_Sans_Condensed } from 'next/font/google';
+
 // STYLES
+const fira_sans_600 = Fira_Sans_Condensed({weight: ['600'], subsets: ['latin']})
+
 const styles = {
     details: {
         padding: '1rem',
@@ -20,40 +25,33 @@ const styles = {
 };
 
 //APP
-export default function ProfileCard(props: any) {
+export default function ProfileCard({petImage}: {petImage: string}) {
+
     return (
-        <Card variant="outlined" style={{ backgroundColor: '#F3DDD1' }}>
-            <Grid container direction="column" justifyContent="center" alignItems="center">
-                {/* CARD HEADER START */}
-                <Grid item sx={{ p: '1.5rem 0rem', textAlign: 'center' }}>
-                    {/* PROFILE PHOTO */}
-                    <Badge
-                        overlap="circular"
-                        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-                        badgeContent={
-                            <PhotoCameraIcon
-                                sx={{
-                                    border: '5px solid white',
-                                    backgroundColor: '#ff558f',
-                                    borderRadius: '50%',
-                                    padding: '.2rem',
-                                    width: 35,
-                                    height: 35,
-                                }}
-                            ></PhotoCameraIcon>
-                        }
-                    >
-                        <Avatar sx={{ width: 200, height: 200, mb: 1.5 }} src={props.media}></Avatar>
-                    </Badge>
-
+        <Card variant="outlined" style={{ height: 400, width: 450, margin: 10, boxShadow: '8px 8px #472F05', backgroundColor: '#F3DDD1',  
+        borderRadius: 0, border: '2px solid #472F05', position: 'relative'}}>
+            {/* <Grid container direction="column" justifyContent="center" alignItems="center" position='relative'>
+                CARD HEADER START */}
+                <Image
+                    src={petImage}
+                    alt="Pet Picture"
+                    fill={true}
+                    style={{
+                        // maxWidth: '-webkit-fill-available',
+                        // maxHeight: 'fit-content',
+                        objectFit: 'cover',
+                        // minHeight: 'fit-content',
+                        // boxShadow: '0 4px 4px -2px #472F05',
+                    }}
+                />
                     {/* DESCRIPTION */}
-
+{/* 
                     <Typography variant="h5">{props.name}</Typography>
                     <Typography variant="h6">{props.sub}</Typography>
                     <Typography color="text.secondary">{props.pet_id}</Typography>
-                    <Typography color="text.secondary">{props.seller_id}</Typography>
-                </Grid>
-            </Grid>
+                    <Typography color="text.secondary">{props.seller_id}</Typography> */}
+                {/* </Grid> */}
+            {/* </Grid> */}
         </Card>
     );
 }
