d2539516508fcc941de073934a5c6a775fdd5dff
Phuvanach Phoemphol <arrmph@gmail.com>
2024-02-17 13:54:51 +0700

    [TL-104] Init File

---
+++ pet/modules/TransactionHistory/TransactionHistoryPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TransactionHistoryPage() {
+    return (
+        <>
+            <h1>Transaction History</h1>
+        </>
+    )
+}
+
+export default TransactionHistoryPage
