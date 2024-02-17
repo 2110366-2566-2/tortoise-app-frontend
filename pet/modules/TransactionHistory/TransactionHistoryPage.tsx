3d2b79fcf3199e459a0dee40dd20a5690d5cdfa4
Phuvanach Phoemphol <arrmph@gmail.com>
2024-02-17 23:40:13 +0700

    [TL-104] Add Loding Component

--- pet/modules/TransactionHistory/TransactionHistoryPage.tsx
+++ pet/modules/TransactionHistory/TransactionHistoryPage.tsx
@@ -1,9 +1,31 @@
+'use client';
+
 import React from 'react'
+import { useEffect, useState } from 'react';
+import CenterLoader from '@components/CenterLoader';
+import useGetTransactionHistory from '@services/api/v1/user/useGetTransactionHistory';
 
 function TransactionHistoryPage() {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await useGetTransactionHistory();
+            setData(response.data);
+            setLoading(response.loading);
+            setError(response.error);
+        };
+        fetchData();
+    }, []);
+
+    if (loading) return <CenterLoader />;
+    // if (error) return <Error />;
+
     return (
         <>
-            <h1>Transaction History</h1>
+
         </>
     )
 }
