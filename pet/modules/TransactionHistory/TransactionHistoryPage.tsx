770e9445a46535b09a5a8607fda5c49aac735469
ARM <arrmph@gmail.com>
2024-02-21 13:30:36 +0700

    [TL-104] Simple component

--- pet/modules/TransactionHistory/TransactionHistoryPage.tsx
+++ pet/modules/TransactionHistory/TransactionHistoryPage.tsx
@@ -3,29 +3,94 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
 import CenterLoader from '@components/CenterLoader';
+import Transaction from '@components/Transaction/index';
+import { Typography, Grid } from '@mui/material';
 import useGetTransactionHistory from '@services/api/v1/user/useGetTransactionHistory';
+import { set } from 'react-hook-form';
 
 function TransactionHistoryPage() {
     const [data, setData] = useState([]);
+    const [role, setRole] = useState(1);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
-            const response = await useGetTransactionHistory();
-            setData(response.data);
+            // Mock API call
+            const response : any = await useGetTransactionHistory();
+            const formattedData = response.data.map((transaction: any) => {
+                if (transaction.payment_method.toLowerCase() === 'paypal') {
+                    transaction.payment_method = 'PAYPAL';
+                } else if (transaction.payment_method.toLowerCase() === 'creditcard') {
+                    transaction.payment_method = 'CREDIT CARD';
+                }
+                if (role === 1) {
+                    return {
+                        ...transaction,
+                        price: '+' + transaction.price.toLocaleString() + ' THB'
+                    }
+                } else if (role === 2) {
+                    return {
+                        ...transaction,
+                        price: '-' + transaction.price.toLocaleString() + ' THB'
+                    }
+                }
+            });
+            setData(formattedData);
+            setRole(response.role);
             setLoading(response.loading);
             setError(response.error);
         };
         fetchData();
-    }, []);
+    }, [data]);
 
     if (loading) return <CenterLoader />;
+    // Never have error ;)
     // if (error) return <Error />;
 
     return (
         <>
-
+            <Typography variant="h5" sx={{ textAlign: 'center', fontWeight: 800, p: '32px 10% 15px' }}>
+                Transaction History
+            </Typography>
+            <Typography variant="subtitle1" sx={{ textAlign: 'center', fontWeight: 500, p: '8px 10% 70px', color: '#808080'}}>
+                From fluff to funds: Tracking your pet-ty transactions! 🐾💰
+            </Typography>
+            <Grid
+                container
+                justifyContent='center'
+                alignItems='center'
+                columns={{xs: 13}}
+                spacing={{xs: 1}}
+            >
+                <Grid item xs={3}>
+                    <Typography variant='subtitle1' sx={{fontWeight: 800, p: '0px 10% 0px', color: '#8b94aa'}}>
+                        DATE & TIME
+                    </Typography>
+                </Grid>
+                <Grid item xs={3}>
+                    <Typography variant='subtitle1' sx={{fontWeight: 800, p: '0px 10% 0px', color: '#8b94aa'}}>
+                        TYPE
+                    </Typography>
+                </Grid>
+                <Grid item xs={2}>
+                    <Typography variant='subtitle1' sx={{fontWeight: 800, p: '0px 10% 0px', color: '#8b94aa'}}>
+                        STATUS
+                    </Typography>
+                </Grid>
+                <Grid item xs={3}>
+                    <Typography variant='subtitle1' sx={{fontWeight: 800, p: '0px 10% 0px', color: '#8b94aa'}}>
+                        AMOUNT
+                    </Typography>
+                </Grid>
+                <Grid item xs={1}>
+                </Grid>
+                {data.map((transaction, index) => (
+                    <Grid item xs={12} key={index}>
+                        <Transaction role={role} transaction={transaction} />
+                    </Grid>
+                ))}
+            </Grid>
         </>
     )
 }
--- pet/services/api/v1/user/useGetTransactionHistory.ts
+++ pet/services/api/v1/user/useGetTransactionHistory.ts
@@ -1,42 +1,101 @@
-function useGetTransactionHistory() {
+type Transaction = {
+    seller_name: string;
+    price: number;
+    pet_name: string;
+    timestamp: {
+        date: string;
+        time: string;
+    };
+    status: string;
+    payment_method: string;
+};
+
+type TransactionResponse = {
+    role: number;
+    data: Transaction[];
+    loading: boolean;
+    error: any;
+};
+
+function useGetTransactionHistory(): Promise<TransactionResponse> {
     // Mock data
-    const mockData = [
-        {
-            "seller_name": "John Doe",
-            "price": 1000,
-            "pet_name": "Buddy",
-            "timestamp": {
-                "date": "2021-10-01",
-                "time": "12:00"
+    const mockData = {
+        role: 1,
+        data: [
+            {
+                seller_name: 'John Doe',
+                price: 1000,
+                pet_name: 'Fluffy',
+                timestamp: {
+                    date: '2021-10-10',
+                    time: '10:00'
+                },
+                status: 'pending',
+                payment_method: 'paypal'
             },
-            "payment_method": "CreditCard"
-        },
-        {
-            "seller_name": "Jane Doe",
-            "price": 2000,
-            "pet_name": "Max",
-            "timestamp": {
-                "date": "2021-10-02",
-                "time": "13:00"
+            {
+                seller_name: 'Jane Doe',
+                price: 500,
+                pet_name: 'Fido',
+                timestamp: {
+                    date: '2021-10-11',
+                    time: '11:00'
+                },
+                status: 'pending',
+                payment_method: 'creditcard'
             },
-            "payment_method": "Paypal"
-        },
-        {
-            "seller_name": "John Doe",
-            "price": 1500,
-            "pet_name": "Buddy",
-            "timestamp": {
-                "date": "2021-10-03",
-                "time": "14:00"
+            {
+                seller_name: 'John Doe',
+                price: 1000,
+                pet_name: 'Fluffy',
+                timestamp: {
+                    date: '2021-10-12',
+                    time: '12:00'
+                },
+                status: 'paid',
+                payment_method: 'paypal'
             },
-            "payment_method": "CreditCard"
-        }
-    ]
+            {
+                seller_name: 'Jane Doe',
+                price: 500,
+                pet_name: 'Fido',
+                timestamp: {
+                    date: '2021-10-13',
+                    time: '13:00'
+                },
+                status: 'paid',
+                payment_method: 'creditcard'
+            },
+            {
+                seller_name: 'John Doe',
+                price: 1000,
+                pet_name: 'Fluffy',
+                timestamp: {
+                    date: '2021-10-14',
+                    time: '14:00'
+                },
+                status: 'paid',
+                payment_method: 'paypal'
+            },
+            {
+                seller_name: 'Jane Doe',
+                price: 500,
+                pet_name: 'Fido',
+                timestamp: {
+                    date: '2021-10-15',
+                    time: '15:00'
+                },
+                status: 'paid',
+                payment_method: 'creditcard'
+            }
+        ],
+    };
 
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve({
-                data: mockData,
+                role: mockData.role,
+                data: mockData.data,
                 loading: false,
                 error: null
             });
