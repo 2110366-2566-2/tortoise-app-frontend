c799027bb9c0915b17d03167131da87ce82336ff
khunnnnnnn <nunnun.234@gmail.com>
2024-02-22 18:41:50 +0700

    [TL-68] Design User Profile Page

--- pet/components/UserSettings/index.tsx
+++ pet/components/UserSettings/index.tsx
@@ -1,23 +1,9 @@
 'use client';
 
+import { Box, Typography, Stack, Card, Divider, FormLabel, FormControl, Avatar } from '@mui/material';
 import React, { useState, useEffect } from 'react';
-import Card from '@mui/material/Card';
-import Divider from '@mui/material/Divider';
-import CardContent from '@mui/material/CardContent';
-import Box from '@mui/material/Box';
-import { Button, Grid } from '@mui/material';
-import FormControl from '@mui/material/FormControl';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Dayjs } from 'dayjs';
-import { IPetDetail } from '../../services/api/v1/pets/type';
-import { DataGrid, GridRowsProp, GridColDef, GridCellParams } from '@mui/x-data-grid';
 import { Fira_Sans_Condensed } from 'next/font/google';
-import { ButtonProps, styled } from '@mui/material';
-import { useRouter } from 'next/navigation';
 
 const fira_sans_600 = Fira_Sans_Condensed({ weight: ['600'], subsets: ['latin'] });
 
@@ -28,6 +14,7 @@ const theme = createTheme({
 });
 export interface IUserDetail {
     _id: any;
+    username: string;
     first_name: string;
     last_name: string;
     gender: string;
@@ -47,74 +34,52 @@ export interface Address {
 }
 
 export default function UserSettingsCard(props: IUserDetail) {
-    const ColorButton = styled(Button)<ButtonProps>(({ theme }) => ({
-        color: theme.palette.getContrastText('#F9C067'),
-        '&:hover': {
-            backgroundColor: '#F79762',
-        },
-    }));
-
-    const router = useRouter();
-
-    const rows: GridRowsProp = [
-        { id: 1, col1: 'FirstName:', col2: props.first_name },
-        { id: 2, col1: 'LastName:', col2: props.last_name },
-        { id: 3, col1: 'Gender:', col2: props.gender },
-        { id: 4, col1: 'PhoneNumber:', col2: props.phoneNumber },
-        { id: 5, col1: 'Address:', col2: getAddressString(props.address) },
-    ];
-    function getAddressString(addresses: Address[]): string {
-        // Iterate over each Address object and concatenate its properties
-        return addresses
-            .map(
-                (address) =>
-                    `${address.houseNumber}, ${address.street}, ${address.subdistrict}, ${address.district}, ${address.province}, ${address.postalCode}`,
-            )
-            .join('\n');
-    }
-
-    const columns: GridColDef[] = [
-        { field: 'col1', headerName: 'Column 1', width: 150 },
-        {
-            field: 'col2',
-            headerName: 'Column 2',
-            renderCell: (params: GridCellParams) => <div style={{ whiteSpace: 'pre-wrap' }}>{'Test'}</div>,
-        },
-    ];
+    
+    const sxFormLabel = {fontFamily: fira_sans_600.style.fontFamily, fontSize: 20, color: '#472F05'}
+    const sxTypography = {fontFamily: fira_sans_600.style.fontFamily, fontSize: 18, paddingLeft: 3, color: 'rgb(72,52,24)'}
 
     return (
-        <Box sx={{ height: 'auto', width: 'auto', margin: 1 }}>
-            <Box sx={{ boxShadow: '6px 6px #472F05' }}>
-                <Box
-                    sx={{
-                        height: 'auto',
-                        paddingLeft: 3,
-                        paddingY: 1,
-                        fontSize: 30,
-                        backgroundColor: '#F9C067',
-                        border: '2px solid #472F05',
-                    }}
-                >
-                    {'MyProfile'}
+        <Box
+            sx={{
+                py: 3,
+                width: '100%',
+                display: 'block',
+                position: 'relative',
+            }}
+        >
+            <Card
+                sx={{ border: '2px solid black', borderRadius: 0, boxShadow: '5px 4px #472F05', backgroundColor: '#F3DDD1', py: '8px' }}
+            >
+                <Box sx={{ mb: 1, paddingLeft: 3, paddingY: 1 }}>
+                    <Typography sx={{ overflow: 'hidden', textWrap: 'nowrap', fontFamily: fira_sans_600.style.fontFamily,
+                    fontSize: 22 }}>
+                        User Information
+                    </Typography>
                 </Box>
-
-                <DataGrid
-                    rows={rows}
-                    columns={columns}
-                    hideFooter={true}
-                    columnHeaderHeight={0}
-                    rowHeight={45}
-                    sx={{
-                        border: '2px solid #472F05',
-                        borderRadius: 0,
-                        fontFamily: fira_sans_600.style.fontFamily,
-                        fontSize: 18,
-                        '& .MuiDataGrid-cell': {
-                            paddingLeft: 3,
-                        },
-                    }}
-                />
-            </Box>
+                <Divider />
+                <Stack direction="row" spacing={5} sx={{ display: 'flex', alignItems: 'center', my: 2, mx: 4 }}>
+                    <Stack spacing={2.5} sx={{ flexGrow: 1 }}>
+                        <Stack spacing={0} sx={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                            <FormLabel sx={sxFormLabel}>Name:</FormLabel>
+                            <FormControl sx={{ display: { sm: 'flex-column', md: 'flex-row' }, gap: 2 }}>
+                                <Typography sx={sxTypography}>{props.first_name} {props.last_name}</Typography>
+                            </FormControl>
+                        </Stack>
+                        <Stack direction="row" spacing={10}>
+                            <FormControl sx={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                                <FormLabel sx={sxFormLabel}>Gender:</FormLabel>
+                                <Typography sx={sxTypography}>{props.gender}</Typography>
+                            </FormControl>
+                        </Stack>
+                        <div>
+                            <FormControl sx={{ display: 'flex' , flexDirection: 'row', alignItems: 'center'  }}>
+                                <FormLabel sx={sxFormLabel}>Timezone:</FormLabel>
+                                <Typography sx={sxTypography}>INDOCHINA</Typography>
+                            </FormControl>
+                        </div>
+                    </Stack>
+                </Stack>
+            </Card>
         </Box>
     );
 }
