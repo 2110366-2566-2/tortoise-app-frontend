40e5d45d4a4bb8d54052c54a38785ebe4ff4afe7
khunnnnnnn <nunnun.234@gmail.com>
2024-02-24 20:59:57 +0700

    [TL-68, TL-69] Restyle User Profile Page

--- pet/components/UserSettings/index.tsx
+++ pet/components/UserSettings/index.tsx
@@ -3,15 +3,15 @@
 import { Box, Typography, Stack, Card, Divider, FormLabel, FormControl, Avatar } from '@mui/material';
 import React, { useState, useEffect } from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Fira_Sans_Condensed } from 'next/font/google';
-
-const fira_sans_600 = Fira_Sans_Condensed({ weight: ['600'], subsets: ['latin'] });
+import { fira_sans_800, fira_sans_600 } from '../../core/theme/theme';
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
 
 const theme = createTheme({
     typography: {
         fontFamily: ['Arial', 'sans-serif'].join(','),
     },
 });
+
 export interface IUserDetail {
     _id: any;
     username: string;
@@ -24,19 +24,35 @@ export interface IUserDetail {
 }
 
 export interface Address {
-    district: string;
-    subdistrict: string;
-    street: string;
-    building: string;
-    houseNumber: string;
-    province: string;
-    postalCode: string;
+    houseNumber: string,
+    building: string,
+    street: string,
+    district: string,
+    subdistrict: string,
+    province: string,
+    postalCode: string,
 }
+
 
 export default function UserSettingsCard(props: IUserDetail) {
     
-    const sxFormLabel = {fontFamily: fira_sans_600.style.fontFamily, fontSize: 20, color: '#472F05'}
+    const sxFormLabel = {fontFamily: fira_sans_800.style.fontFamily, fontSize: 20, color: '#472F05', py: 0.5}
     const sxTypography = {fontFamily: fira_sans_600.style.fontFamily, fontSize: 18, paddingLeft: 3, color: 'rgb(72,52,24)'}
+
+    const rows: GridRowsProp = [
+        { id: 1, col1: 'House Number:', col2: props.address[0].houseNumber + " " + props.address[0].building},
+        // { id: 2, col1: 'Building:', col2: props.address[0].building},
+        { id: 3, col1: 'Street:', col2:props.address[0].street },
+        { id: 4, col1: 'Sub-District:', col2: props.address[0].subdistrict },
+        { id: 5, col1: 'District:', col2: props.address[0].district },
+        { id: 6, col1: 'Province:', col2: props.address[0].province},
+        { id: 7, col1: 'Postal Code:',   col2: props.address[0].postalCode },
+    ];
+
+    const columns: GridColDef[] = [
+        { field: 'col1', headerName: 'Column 1', width: 200 },
+        { field: 'col2', headerName: 'Column 2', width: 300 },
+    ];
 
     return (
         <Box
@@ -48,35 +64,124 @@ export default function UserSettingsCard(props: IUserDetail) {
             }}
         >
             <Card
-                sx={{ border: '2px solid black', borderRadius: 0, boxShadow: '5px 4px #472F05', backgroundColor: '#F3DDD1', py: '8px' }}
+                sx={{ 
+                    border: '2px solid black', 
+                    borderRadius: 1, 
+                    boxShadow: '10px 10px #472F05', 
+                    backgroundColor: '#F3DDD1', 
+                    py: '8px' 
+                }}
             >
                 <Box sx={{ mb: 1, paddingLeft: 3, paddingY: 1 }}>
-                    <Typography sx={{ overflow: 'hidden', textWrap: 'nowrap', fontFamily: fira_sans_600.style.fontFamily,
-                    fontSize: 22 }}>
+                    <Typography sx={{ overflow: 'hidden', textWrap: 'nowrap', fontFamily: fira_sans_800.style.fontFamily,
+                    fontSize: 25, color: '#472F05' }}>
                         User Information
                     </Typography>
                 </Box>
+
                 <Divider />
-                <Stack direction="row" spacing={5} sx={{ display: 'flex', alignItems: 'center', my: 2, mx: 4 }}>
+
+                <Stack 
+                    direction="row" 
+                    spacing={5} 
+                    sx={{ 
+                        display: 'flex', 
+                        alignItems: 'center', 
+                        my: 2, mx: 4 
+                    }}
+                >
                     <Stack spacing={2.5} sx={{ flexGrow: 1 }}>
-                        <Stack spacing={0} sx={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-                            <FormLabel sx={sxFormLabel}>Name:</FormLabel>
+
+                        <Stack 
+                            spacing={0} 
+                            sx={{
+                                display: 'flex', 
+                                flexDirection: 'row', 
+                                alignItems: 'center'
+                            }}
+                        >
+                            <FormLabel sx={sxFormLabel}> 
+                                Name: 
+                            </FormLabel>
                             <FormControl sx={{ display: { sm: 'flex-column', md: 'flex-row' }, gap: 2 }}>
-                                <Typography sx={sxTypography}>{props.first_name} {props.last_name}</Typography>
+                                <Typography sx={sxTypography}>
+                                    {props.first_name} {props.last_name}
+                                </Typography>
                             </FormControl>
                         </Stack>
-                        <Stack direction="row" spacing={10}>
-                            <FormControl sx={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-                                <FormLabel sx={sxFormLabel}>Gender:</FormLabel>
-                                <Typography sx={sxTypography}>{props.gender}</Typography>
+
+                        <Stack 
+                            spacing={0} 
+                            sx={{
+                                display: 'flex', 
+                                flexDirection: 'row', 
+                                alignItems: 'center'
+                            }}
+                        >
+                            <FormLabel sx={sxFormLabel}> 
+                                Gender: 
+                            </FormLabel>
+                            <FormControl sx={{ display: { sm: 'flex-column', md: 'flex-row' }, gap: 2 }}>
+                                <Typography sx={sxTypography}>
+                                    {props.gender}
+                                </Typography>
                             </FormControl>
                         </Stack>
-                        <div>
-                            <FormControl sx={{ display: 'flex' , flexDirection: 'row', alignItems: 'center'  }}>
-                                <FormLabel sx={sxFormLabel}>Timezone:</FormLabel>
-                                <Typography sx={sxTypography}>INDOCHINA</Typography>
+
+                        <Stack 
+                            spacing={0} 
+                            sx={{
+                                display: 'flex', 
+                                flexDirection: 'row', 
+                                alignItems: 'center'
+                            }}
+                        >
+                            <FormLabel sx={sxFormLabel}> 
+                                Tel: 
+                            </FormLabel>
+                            <FormControl sx={{ display: { sm: 'flex-column', md: 'flex-row' }, gap: 2 }}>
+                                <Typography sx={sxTypography}>
+                                    {props.phoneNumber}
+                                </Typography>
                             </FormControl>
-                        </div>
+                        </Stack>
+
+                        <Box>
+                             <Stack 
+                                spacing={0}
+                                sx={{
+                                    display: 'flex', 
+                                    flexDirection: 'column', 
+                                    alignItems: 'start',
+                                }}
+                            >
+                                <FormLabel sx={sxFormLabel}> 
+                                    Address: 
+                                </FormLabel>
+                                <DataGrid
+                                    rows={rows}
+                                    columns={columns}
+                                    hideFooter={true}
+                                    columnHeaderHeight={0}
+                                    rowHeight={45}
+                                    sx={{
+                                        mx: 3,
+                                        width: '90%',
+                                        border: '5px double #472F05',
+                                        borderRadius: 0,
+                                        boxShadow: '3px 3px #472F05',
+                                        fontFamily: fira_sans_600.style.fontFamily,
+                                        fontSize: 18,
+                                        color: '#472F05',
+                                        '& .MuiDataGrid-cell': {
+                                            paddingLeft: 3,
+                                        },
+                                    }}
+                                />
+                            </Stack>
+                        </Box>
+                       
+                        
                     </Stack>
                 </Stack>
             </Card>
--- pet/modules/UserProfile/UserProfilePage.tsx
+++ pet/modules/UserProfile/UserProfilePage.tsx
@@ -4,49 +4,27 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import { Avatar, Typography } from '@mui/material';
 import { useRouter } from 'next/navigation';
-import UserProfileCard from '../../components/UserProfileCard';
 import UserSettingsCard, { IUserDetail } from '../../components/UserSettings';
-import { fira_sans_600 } from '../../core/theme/theme';
-import ListSubheader from '@mui/material/ListSubheader';
-import List from '@mui/material/List';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import PasswordIcon from '@mui/icons-material/Password';
-import DeleteIcon from '@mui/icons-material/Delete';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import Collapse from '@mui/material/Collapse';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
-import StarBorder from '@mui/icons-material/StarBorder';
-import Image from 'next/image';
+import { fira_sans_800 } from '../../core/theme/theme';
 import AccountOptionList from '../../components/AccountOptionList';
 
 export default function UserProfilePage() {
-    const router = useRouter();
-    const [hover, setHover] = useState(false);
-    const [open, setOpen] = React.useState(true);
-    // const handleClick = () => {
-    //     setOpen(!open);
-    // };
 
     const mockUserDetail: IUserDetail = {
         _id: '123456789', // example ID
         username: 'ruthlessYeddo',
-        first_name: 'John',
-        last_name: 'Doe',
+        first_name: 'Rutthee',
+        last_name: 'SudtreenloeyPree',
         gender: 'Male',
         phoneNumber: '123-456-7890',
         image: 'https://drive.google.com/uc?id=1xI9x6kY2nBrd16MQhqLX9mG1e1XIDOLt',
         address: [
-            {
+            { 
+                houseNumber: '123',
+                building: ' Building',
+                street: 'Street',
                 district: 'District',
                 subdistrict: 'Subdistrict',
-                street: 'Street',
-                building: ' Building',
-                houseNumber: '123',
                 province: 'Province',
                 postalCode: '12345',
             },
@@ -55,38 +33,21 @@ export default function UserProfilePage() {
 
     return (
         <Box sx={{ alignSelf: 'center', mt: 5 }}>
-            {/* <Box sx={{mb: 1.5, mt: 20, display: 'flex', flexDirection: 'row', position: 'absolute', 
-                alignItems: 'end', height: 200}}>
-                <Avatar sx={{ width: 180 , height: 180, ml: {xs: 10, md: 20}, mr: {xs: 3, md: 7}, border: '4px solid #472F05', boxShadow: 10 }} />
-                <Box sx={{display: 'block'}}>
-                    <Typography sx={{fontFamily: fira_sans_600.style.fontFamily, fontSize: 30}}>{mockUserDetail.username}</Typography>
-                    <Typography sx={{fontFamily: fira_sans_600.style.fontFamily, fontSize: 16}}>(Email)</Typography>
-                    <Typography sx={{fontFamily: fira_sans_600.style.fontFamily, fontSize: 16}}>Tel: {mockUserDetail.phoneNumber}</Typography>
-                </Box>
-            </Box>
-            <Box sx={{height: 250, backgroundColor: '#FFF8E8', position: 'relative', zIndex: -10}}>
-                <Image 
-                    src={mockUserDetail.image}
-                    alt='banner'
-                    fill={true}
-                    objectFit='cover'
-                />
-            </Box>
-            <Box sx={{height: 140, backgroundColor: '#E5CB9A'}}> </Box> */}
 
             <Grid container direction="column" sx={{ overflowX: 'hidden', flexWrap: 'nowrap' }}>
                 <Grid
                     container
                     direction={ { xs: 'column', md: 'row' } }
-                    spacing={10}
+                    spacing={{xs: 0, md: 10}}
                     sx={{
                         display: 'flex',
                         flexWrap: 'nowrap',
                         top: '15vh',
                         px: { xs: 7, md: 12 },
+                        pt: 0
                     }}
                 >
-                    <Grid item md={4} sx={{ alignSelf: 'normal', justifySelf: 'center', mt: 0}}>
+                    <Grid item md={4} sx={{ alignSelf: 'normal', justifySelf: 'center', mt: 3}}>
                         <Box display={'flex'} flexDirection={'column'} alignItems={'center'} pb={3} >
                             <Box 
                                 display={'flex'} 
@@ -94,18 +55,27 @@ export default function UserProfilePage() {
                                 alignItems={'center'} 
                                 py={3}
                                 px={4}
-                                boxShadow={'3px 3px #472F05'}
-                                border={'2px solid #472F05'}
-                                borderRadius={1}>
+                                border={'3px solid #472F05'}
+                                boxShadow={'6px 6px #472F05'}
+                                borderRadius={0}
+                                sx={{
+                                    backgroundColor: '#E5CB9A'
+                                }}
+                            >
                                 <Avatar 
+                                    alt='Profile Picture'
+                                    src={mockUserDetail.image}
                                     sx={{ 
                                         width: 180 , height: 180,  
-                                        border: '4px solid #472F05', 
+                                        border: '2px solid #472F05', 
                                         boxShadow: 10,
-                                         
+                                        fontSize: 70,
+                                        fontFamily: fira_sans_800.style.fontFamily
                                     }} 
-                                />
-                                <Typography variant='h5' fontFamily={fira_sans_600.style.fontFamily} color={'#472F05'} pt={5}>
+                                >
+                                    {mockUserDetail.username[0].toUpperCase()}
+                                </Avatar>
+                                <Typography variant='h5' fontFamily={fira_sans_800.style.fontFamily} color={'#472F05'} pt={5}>
                                     {mockUserDetail.username}
                                 </Typography>
                             </Box>
@@ -115,7 +85,7 @@ export default function UserProfilePage() {
                         </Box>
                     </Grid>
 
-                    <Grid item md={8} sx={{ width: '100%', alignSelf: 'normal', right: 0}}>
+                    <Grid item md={7.5} sx={{ width: '100%', alignSelf: 'normal', right: 0}}>
                         <UserSettingsCard
                             _id={mockUserDetail._id}
                             username={mockUserDetail.username}
