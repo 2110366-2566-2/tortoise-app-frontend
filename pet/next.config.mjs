85cb9da402089f272d61723afa37e2cc1fb139d6
khunnnnnnn <nunnun.234@gmail.com>
2024-02-28 16:45:03 +0700

    [FIX] New config of images domain
    The images can be now fetched from any domain.

--- pet/next.config.mjs
+++ pet/next.config.mjs
@@ -7,6 +7,12 @@ const nextConfig = {
     basePath: '/petpal',
     images: {
         domains: ['drive.google.com', 'picsum.photos', 'cdn2.thedogapi.com', 'scontent.fbkk5-7.fna.fbcdn.net', 'images.dog.ceo'],
+        remotePatterns: [
+            {
+              protocol: "https",
+              hostname: "**",
+            },
+          ],
     },
     webpack: (config) => {
         config.resolve.alias.canvas = false;
